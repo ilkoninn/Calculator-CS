a9bb7e8b907ede917ad96a65e6b57790ab62a9ed
Recebov Ilkin <recebovilkin256@gmail.com>
2023-10-08 10:13:14 +0400

    New MyStringClass file created and other files have been updated.

---
+++ Calculator/MyStringClass.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class MyStringClass : MyClass
+    {
+
+        public string ToLowerString(ref string text)
+        {
+            char[] lowerAlphabet = {
+                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+            };
+
+            char[] upperAlphabet = {
+                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+                'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+            };
+
+            string[] newString = new string[text.Length];
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                for (int j = 0; j < upperAlphabet.Length; j++)
+                {
+                    if (text[i] == upperAlphabet[j])
+                    {
+                        newString[i] += lowerAlphabet[j];
+                    }
+                    if (text[i] == lowerAlphabet[j])
+                    {
+                        newString[i] += lowerAlphabet[j];
+                    }
+                }
+            }
+
+            text = "";
+            for (int i = 0; i < newString.Length; i++) text += newString[i];
+            return text;
+        }
+    }
+}
--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             int count = 0;
+            bool running = true;
 
             Console.WriteLine("=================== Calculator Program ===================");
             do
@@ -21,97 +22,117 @@
                     "\n 5 - Power" +
                     "\nExit(Any number)" +
                     "\n\nPlease choose a operation ID(1-5): ");
+                
                 int userChoice = int.Parse(Console.ReadLine());
 
-                MyClass operation = new MyClass();
-                double answer = 0;
-                if (userChoice == 1)
+                switch (userChoice)
                 {
-                    Console.Write("\nFirst number: ");
-                    double firstNumber = double.Parse(Console.ReadLine());
+                    case 1:
+                        Console.Write("\nFirst number: ");
+                        double firstNumber = double.Parse(Console.ReadLine());
 
-                    Console.Write("Second number: ");
-                    double secondNumber = double.Parse(Console.ReadLine());
+                        Console.Write("Second number: ");
+                        double secondNumber = double.Parse(Console.ReadLine());
 
-                    answer = operation.Addition(firstNumber, secondNumber);
+                        Console.WriteLine("Addition of these numbers: " + GetAnswer(firstNumber, secondNumber, userChoice) + "\n");
+                        break;
+                    case 2:
+                        Console.Write("\nFirst number: ");
+                        firstNumber = double.Parse(Console.ReadLine());
 
-                    Console.WriteLine("Addition of these numbers: " + answer + "\n");
+                        Console.Write("Second number: ");
+                        secondNumber = double.Parse(Console.ReadLine());
 
-                }
-                else if (userChoice == 2)
-                {
-                    Console.Write("\nFirst number: ");
-                    double firstNumber = double.Parse(Console.ReadLine());
+                        Console.WriteLine("Subtraction of these numbers: " + GetAnswer(firstNumber, secondNumber, userChoice) + "\n");
+                        break;
+                    case 3:
+                        Console.Write("\nFirst number: ");
+                        firstNumber = double.Parse(Console.ReadLine());
 
-                    Console.Write("Second number: ");
-                    double secondNumber = double.Parse(Console.ReadLine());
+                        Console.Write("Second number: ");
+                        secondNumber = double.Parse(Console.ReadLine());
 
-                    answer = operation.Subtraction(firstNumber, secondNumber);
+                        Console.WriteLine("Multiplication of these numbers: " + GetAnswer(firstNumber, secondNumber, userChoice) + "\n");
+                        break;
+                    case 4:
+                        Console.Write("\nFirst number: ");
+                        firstNumber = double.Parse(Console.ReadLine());
 
-                    Console.WriteLine("Subtraction of these numbers: " + answer + "\n");
-                }
-                else if (userChoice == 3)
-                {
-                    Console.Write("\nFirst number: ");
-                    double firstNumber = double.Parse(Console.ReadLine());
+                        Console.Write("Second number: ");
+                        secondNumber = double.Parse(Console.ReadLine());
 
-                    Console.Write("Second number: ");
-                    double secondNumber = double.Parse(Console.ReadLine());
-
-                    answer = operation.Multiplication(firstNumber, secondNumber);
-
-                    Console.WriteLine("Multiplication of these numbers: " + answer + "\n");
-                }
-                else if (userChoice == 4)
-                {
-                    Console.Write("\nFirst number: ");
-                    double firstNumber = double.Parse(Console.ReadLine());
-
-                    Console.Write("Second number: ");
-                    double secondNumber = double.Parse(Console.ReadLine());
-
-                    answer = operation.Division(firstNumber, secondNumber);
-
-                    Console.WriteLine("Division of these numbers: " + answer + "\n");
-                }
-                else if (userChoice == 5)
-                {
-                    Console.WriteLine("\nIf you want to find power of numbers, you should look that:" +
+                        Console.WriteLine("Division of these numbers: " + GetAnswer(firstNumber, secondNumber, userChoice) + "\n");
+                        break;
+                    case 5:
+                        Console.WriteLine("\nIf you want to find power of numbers, you should look that:" +
                         "\nThe first number will be your number, which find its power");
-                    Console.Write("\nFirst number: ");
-                    double firstNumber = double.Parse(Console.ReadLine());
+                        Console.Write("\nFirst number: ");
+                        firstNumber = double.Parse(Console.ReadLine());
 
-                    Console.Write("Second number: ");
-                    double secondNumber = double.Parse(Console.ReadLine());
+                        Console.Write("Second number: ");
+                        secondNumber = double.Parse(Console.ReadLine());
 
-                    answer = operation.Power(firstNumber, secondNumber);
+                        Console.WriteLine(firstNumber + "to the power of " + secondNumber + " (" +
+                            firstNumber + "^" + secondNumber + "): " + GetAnswer(firstNumber, secondNumber, userChoice) + "\n");
 
-                    Console.WriteLine(firstNumber + "to the power of " + secondNumber + " (" +
-                        firstNumber + "^" + secondNumber + "): " + answer + "\n");
-
+                        break;
+                    default:
+                        running = false;
+                        break;
                 }
-                else
+
+                if (running)
                 {
-                    break;
+                    Console.Write("Do you want to continue?(y/n): ");
+                    string userContinueChoice = Console.ReadLine();
+                    MyStringClass stringOperation = new MyStringClass();
+                    
+                    if (stringOperation.ToLowerString(ref userContinueChoice) == "y" || 
+                        stringOperation.ToLowerString(ref userContinueChoice) == "yes")
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        break;
+                    }
                 }
 
-                Console.Write("Do you want to continue?(y/n): ");
-                string userContinueChoice = Console.ReadLine();
 
-                if (operation.ToLowerString(ref userContinueChoice) == "y" || operation.ToLowerString(ref userContinueChoice) == "yes")
-                {
-                    continue;
-                }
-                else
-                {
-                    break;
-                }
-
-            } while (true);
+            } while (running);
             Console.WriteLine("\n\t\tProgram has been stopped\n");
             Console.WriteLine("==========================================================");
 
+        }
 
+        static double GetAnswer(double firstNumber,  double secondNumber, int userInput)
+        {
+            double answer = 0;
+            MyClass operation = new MyClass();
+
+            
+            switch (userInput)
+            {
+                case 1:
+                    answer = operation.Addition(firstNumber, secondNumber);
+                    break;
+                case 2:
+                    answer = operation.Subtraction(firstNumber, secondNumber);
+                    break;
+                case 3:
+                    answer = operation.Multiplication(firstNumber, secondNumber);
+                    break;
+                case 4:
+                    answer = operation.Division(firstNumber, secondNumber);
+                    break;
+                case 5:
+                    answer = operation.Power(firstNumber, secondNumber);
+                    break;
+                default:
+                    break;
+            }
+
+            return answer;
         }
     }
 }
